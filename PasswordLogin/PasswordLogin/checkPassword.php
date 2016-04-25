1c69ba9d8acaba62279807962ddd1bd62e103ca2
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2016-04-25 09:38:00 +0200

    Update checkPassword.php

--- PasswordLogin/PasswordLogin/checkPassword.php
+++ PasswordLogin/PasswordLogin/checkPassword.php
@@ -1,1 +1,16 @@
+<?php
+/***********************
+ * Davide Ramondetti *
+ * Password Login *
+ * Try it now! *
+************************/
 
+include('config.php'); // include the configuration
+include('password.php'); // include the passwords
+
+if (in_array($_POST['password'],$Passwords)) {
+  header("Location: ".$LockedFile);
+} else {
+  echo "#AdvancedPasswordLogin by Davide Ramondetti : @PASSWORD_ERROR";
+}
+?>
