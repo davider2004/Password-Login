5e8857d802abda525ed9dad710c25e649bf1ade0
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2016-04-25 09:34:04 +0200

    Added the "Submit AntiCheat" code
    Added the $buttonToCheck for delete the cheats in the code!

--- PasswordLogin/index.php
+++ PasswordLogin/index.php
@@ -1,2 +1,32 @@
+<?php
+/**********************
+ * Davide Ramondetti *
+ * PassLogin System *
+ * Try it now! *
+***********************/
+$buttonToCheck = $_POST['submit'];
+if ($buttonToCheck) {
+  include('PasswordLogin/checkPassword.php');
+} else {
+?>
+
 <html>
+  <head>
+    <title>Login Zone</title>
+  </head>
+  <body>
+    <center>
+      <script>
+        // there isn't a design ,you can add it!
+      </script>
+      <form method="POST" action="?">
+        <h1>Login</h1>
+        <p>Username: <b>XX</b></p>
+        <p>Password: <input type="password" name="password" id="password"></p>
+        <p><input type="submit" value="Login" name="submit" id="submit"></p>
+      </form>
+    </center>
+  </body>
 </html>
+
+<?php } ?>
